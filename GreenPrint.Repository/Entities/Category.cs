c5acd3546cea46a612bffb29165e04872fa5a0b2
V4LVE <Alex802c@gmail.com>
2024-03-27 12:40:29 +0100

    Added Entities

---
+++ GreenPrint.Repository/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenPrint.Repository.Entities
+{
+    public class Category
+    {
+    }
+}
