28c14f4fdd3da65f4264a59292e4eefffe3d823a
Alexander Colmorn <Alex802c@gmail.com>
2024-03-30 12:29:04 +0100

    Added DTO mappings using AutoMapper

--- GreenPrint.Service/Services/MappingService.cs
+++ GreenPrint.Service/Services/MappingService.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using GreenPrint.Repository.Entities;
+using GreenPrint.Service.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,8 +18,32 @@ namespace GreenPrint.Services.Services
         {
             AutoMapper.MapperConfiguration config = new AutoMapper.MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<ToDoItem, ToDoItemDTO>();
-                cfg.CreateMap<ToDoItemDTO, ToDoItem>();
+                cfg.CreateMap<Address, AddressDTO>();
+                cfg.CreateMap<AddressDTO, Address>();
+
+                cfg.CreateMap<Category, CategoryDTO>();
+                cfg.CreateMap<CategoryDTO, Category>();
+
+                cfg.CreateMap<Customer, CustomerDTO>();
+                cfg.CreateMap<CustomerDTO, Customer>();
+
+                cfg.CreateMap<Item, ItemDTO>();
+                cfg.CreateMap<ItemDTO, Item>();
+
+                cfg.CreateMap<Order, OrderDTO>();
+                cfg.CreateMap<OrderDTO, Order>();
+
+                cfg.CreateMap<Role, RoleDTO>();
+                cfg.CreateMap<RoleDTO, Role>();
+
+                cfg.CreateMap<User, UserDTO>();
+                cfg.CreateMap<UserDTO, User>();
+
+                cfg.CreateMap<Warehouse, WarehouseDTO>();
+                cfg.CreateMap<WarehouseDTO, Warehouse>();
+
+                cfg.CreateMap<WarehouseItem, WarehouseItemDTO>();
+                cfg.CreateMap<WarehouseItemDTO, WarehouseItem>();
             });
 
             try
